38ed905350b6fb6e787ac909ec5fbfc8f670af4d
sunnywong <tosunnywong@gmail.com>
2018-10-26 17:35:30 +0800

    flask api

--- app.py
+++ app.py
@@ -228,4 +228,5 @@ def create_task():
     return jsonify(response), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.debug = True
+    app.run(host = '0.0.0.0',port=5001)
